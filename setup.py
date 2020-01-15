2deec3f86e77a79d3d352d78f3716f4b70ea1321
André Fonseca <11467925+dafonse@users.noreply.github.com>
2020-01-15 14:18:49 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='scripts',
+      version='0.1',
+      description='Some scripts and smal projects with python',
+      url='https://github.com/dafonse/python-scripts',
+      author='André Fonseca',
+      author_email='github/dafonse',
+      license='MIT',
+      packages=['buscaFilme'],
+      zip_safe=False)
